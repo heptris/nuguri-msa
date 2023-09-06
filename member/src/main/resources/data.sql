bcbb9896bd0f3dea65d159b78c8013a238b8b402
이승호 <dltmdgh579@gmail.com>
2023-09-07 00:49:48 +0900

    docs: test data insert (#30)
    Signed-off-by: 이승호 <dltmdgh579@gmail.com>

--- member/src/main/resources/data.sql
+++ member/src/main/resources/data.sql
@@ -4,4 +4,4 @@ values (now(), now(), 25, 'aaa@naver.com', '$2a$10$50S6QGa1nvtqO8QJfBfwaOXqm.UVN
        (now(), now(), 22, 'ccc@naver.com', '$2a$10$50S6QGa1nvtqO8QJfBfwaOXqm.UVNTE/ljolYa.L1TuapxsO8Awju', 'ccc', 'CCC', null, 'w', 40.5, 3),
        (now(), now(), 23, 'ddd@naver.com', '$2a$10$50S6QGa1nvtqO8QJfBfwaOXqm.UVNTE/ljolYa.L1TuapxsO8Awju', 'ddd', 'DDD', null, 'm', 50.5, 950),
        (now(), now(), 28, 'eee@naver.com', '$2a$10$50S6QGa1nvtqO8QJfBfwaOXqm.UVNTE/ljolYa.L1TuapxsO8Awju', 'eee', 'EEE', null, 'w', 60.0, 4),
-       (now(), now(), 21, 'test@naver.com', '$2a$10$50S6QGa1nvtqO8QJfBfwaOXqm.UVNTE/ljolYa.L1TuapxsO8Awju', 'test', 'test', null, 'w', 38.0, 300);
+       (now(), now(), 21, 'test@naver.com', '$2a$10$50S6QGa1nvtqO8QJfBfwaOXqm.UVNTE/ljolYa.L1TuapxsO8Awju', 'testName', 'testNick', null, 'm', 36.5, 932);
