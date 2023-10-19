19630d35e3350602afd644f1b54e0204a9e444d1
이승호 <dltmdgh579@gmail.com>
2023-10-19 09:48:38 +0900

    feat: Spring Cloud Config Client Watch 적용 (#56)
    Signed-off-by: 이승호 <dltmdgh579@gmail.com>

--- member/src/main/java/nuguri/nuguri_member/NuguriMemberApplication.java
+++ member/src/main/java/nuguri/nuguri_member/NuguriMemberApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableScheduling
 public class NuguriMemberApplication {
 
     public static void main(String[] args) {
